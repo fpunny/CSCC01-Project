db7b7703c478aa9b115356ae8d3d243b87526690
Chaoyue Xi <chaoyue.xi@mail.utoronto.ca>
2018-11-21 17:20:39 -0500

    finish postReport(not tested)

--- Back-end/src/controllers/reportsController.js
+++ Back-end/src/controllers/reportsController.js
@@ -1,5 +1,24 @@
 import { database, getObjectId } from "../util";
 
+const runQuery = async (qid) => {
+  let _id;
+  try {
+    _id = getObjectId(qid);
+  } catch (err) {
+    return
+  }
+  const db = await database.connect();
+  const data = await db.collection('queries').findOne({ _id }, { query: 1 });
+  if (data) {
+    const result = await db.collection('submissions').aggregate(JSON.parse(data.query)).toArray();
+    if (result) {
+      db.close();
+      return result;
+    }
+  }
+  db.close();
+}
+
 export const reportsController = {
 
     getReports: async (res, req) => {
@@ -17,6 +36,33 @@ export const reportsController = {
   
     postReport: async (res, req) => {
       const { user: { sudo }, params: { layout } } = req;
+      let _id;
+
+      if (sudo) {
+        try {
+          _id = getObjectId(layout);
+        } catch (err) {
+          res.status(401).json({ status: 'error', err });
+        }
+  
+        const db = await database.connect();
+        const data = await db.collection('layouts').findOne({ _id }, { queries: 1 });
+        if (data) {
+          for (query in data) {
+            query = runQuery(query);
+          }
+        }
+        
+        const { insertedId } = await db.collection('reports').insertOne(data);
+        if (insertedId) {
+          res.json({ status: 'success', data: { _id: insertedId, ...data } });
+        } else {
+          res.status(401).json({ status: 'error', err: 'Unexpected error for report creation' });
+        }
+        db.close();
+      } else {
+        res.status(403).json({ status: 'error', err: 'Permission Denied' });
+      }
     },
   
     getReport: async (res, req) => {
